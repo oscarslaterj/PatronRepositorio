2af1d52936c0b14fd362c49394dbde7c885a5c5e
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-10-08 19:49:09 -0400

    Haciendo cambios

---
+++ PatronRepositorio/BLL/PresupuestoRepositorio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronRepositorio.BLL
+{
+  /*  public class PresupuestoRepositorio : RepositorioBase<Presupuestos>
+    {
+        public override bool Guardar(Presupuestos presupuesto)
+        {
+            foreach (var item in presupuesto.Detalle)
+            {
+                this.Acumular(item.TipoEgresoId, item.Monto);
+            }
+
+            bool paso = base.Guardar(presupuesto);
+
+            return paso;
+        }
+       
+    }*/
+}
---
+++ PatronRepositorioTests1/BLL/RepositorioBaseTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PatronRepositorio.BLL;
+using PatronRepositorio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronRepositorio.BLL.Tests
+{
+    [TestClass()]
+    public class RepositorioBaseTests
+    {
+        [TestMethod()]
+        public void RepositorioBaseTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Guardar()
+        {
+            RepositorioBase<Cuentas> repositorio;
+            repositorio = new RepositorioBase<Cuentas>();
+            Assert.IsTrue(repositorio.Guardar(GetCuenta()));
+        }
+
+        private Cuentas GetCuenta()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DisposeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
