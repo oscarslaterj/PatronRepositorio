f9dea103a286371560b21783ba1fadcbb4716924
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-10-08 19:15:13 -0400

    Creando interfaz y contexto

---
+++ PatronRepositorio/BLL/RepositorioBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PatronRepositorio.BLL;
+
+namespace PatronRepositorio.BLL
+{
+    public class RepositorioBase<T> : IDisposable, IRepository<T> where T:class
+    {
+        internal Contexto _contexto;
+    }
+}
